9098b1c01f296c8c62cd97a11dcce938855926f4
=baichonghua <18211167516@163.com>
2020-04-13 17:58:19 +0800

    334

--- LeetCode/String/LeetCode 334 反转字符串.php
+++ LeetCode/String/LeetCode 334 反转字符串.php
@@ -1,4 +1,5 @@
 <?php
+namespace Leet\String;
 /**
  * @category 给定一个字符串数组，编写一个方法，原地修改数组，反转数组
  * @author baichonghua <18211167516@163.com>
@@ -10,13 +11,31 @@
  * left == right 说明数组长度是奇数，中间点无需翻转
  * left > right 说明left指针已过中间节点，无需翻转
  * 
- * 
+ * 时间复杂度 O(n/2) 空间复杂度O(n) 调用栈
  * 
  * 
  * 
  * @thinking2
+ * 
+ * 迭代：设定双指针left、right
+ * 循环（left<right）
+ *  对调left和right的值
+ * 
+ * 时间复杂度 O(n) 空间复杂度O(1)
  */
 class Solution {
+
+    function reverseString2(&$s){
+        $count = count($s);
+        if($count<=1) return $s;
+        $left = 0;$right = $count-1;
+        while($left<$right){
+            list($s[$left],$s[$right]) = [$s[$right],$s[$left]];
+            $left++;
+            $right--;
+        }
+        return $s;
+    }
 
     /**
      * @param String[] $s
@@ -40,4 +59,6 @@ class Solution {
 $a = new Solution();
 $s = ['a','b','c'];
 print_r($a->reverseString($s));
+$s = ['a','b','c'];
+print_r($a->reverseString2($s));
 
